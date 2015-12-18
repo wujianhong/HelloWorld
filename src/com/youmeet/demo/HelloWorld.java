2504c2a28c7745de12300b9f0349277fbc84052c
wujianhong <996267036@qq.com>
2015-12-18 12:46:25 +0800

    ad

---
+++ src/com/youmeet/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.youmeet.demo;
+
+/**
+ * Created by jian_pan on 2015/12/18.
+ */
+public class HelloWorld {
+//  main
+   public static void main(String[] args){
+    System.out.println("Hello World");
+
+   }
+}
